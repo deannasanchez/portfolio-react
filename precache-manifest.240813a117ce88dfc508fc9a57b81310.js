8596d8f9c043e5ebbbc086149cc4ee0f83ef4f09
Dee Lynn <deannalguzman@gmail.com>
2020-02-13 20:32:24 -0800

    Updates

---
+++ precache-manifest.240813a117ce88dfc508fc9a57b81310.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9fdd4caf41f80fad104d53262a7526",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "4efc9365bc47d68a1bc1",
+    "url": "/portfolio-react/static/css/main.1330f61d.chunk.css"
+  },
+  {
+    "revision": "4121c1507ceefede508f",
+    "url": "/portfolio-react/static/js/2.c5c4b7db.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-react/static/js/2.c5c4b7db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4efc9365bc47d68a1bc1",
+    "url": "/portfolio-react/static/js/main.5c26334d.chunk.js"
+  },
+  {
+    "revision": "8b82897210e4dbc48f5e",
+    "url": "/portfolio-react/static/js/runtime-main.1786e92c.js"
+  },
+  {
+    "revision": "2a0dff89afed42efb211800066a9e226",
+    "url": "/portfolio-react/static/media/colors-bg.2a0dff89.png"
+  },
+  {
+    "revision": "ad7ca7a818029c14273836a08fdc72d6",
+    "url": "/portfolio-react/static/media/ds-simple-logo.ad7ca7a8.png"
+  },
+  {
+    "revision": "69c068757b1d59ac0e54b3d08a187341",
+    "url": "/portfolio-react/static/media/github-logo-transparent.69c06875.png"
+  },
+  {
+    "revision": "ef6f4fa02a18b86a774956fa1ac7c2fc",
+    "url": "/portfolio-react/static/media/icecoldbeats.ef6f4fa0.png"
+  },
+  {
+    "revision": "2d7fff1ae28ce67f5d09ab4e3d870989",
+    "url": "/portfolio-react/static/media/profile-img.2d7fff1a.png"
+  }
+]);
